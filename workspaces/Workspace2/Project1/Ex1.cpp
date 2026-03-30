29e9d01c16934b6c2833eba2b63f7d76754811f5
Daniel Adelugba <psyda7@nottingham.ac.uk>
2026-03-30 22:26:58 +0100

    Added files

---
+++ workspaces/Workspace2/Project1/Ex1.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+
+using namespace std;
+
+int main()
+{
+    int val; 
+    cout << "Enter your favourite number between 1 and 100:" << endl;
+    
+    cin >> val;
+    
+    cout << "Amazing!! Thats my favourite number too!" << endl; 
+    
+    
+    cout << "No Really! " << val << " is my favourite number too !" << endl;
+    
+    return 0;
+}
